492d36640f1a63fd601632eb5b5f98348ffc9265
Cayke-Chen <caykechen3@gmail.com>
2026-08-25 18:54:08 -0300

    MergeSort e Tela implementados

--- src/gui/telas/TelaMergeSort.java
+++ src/gui/telas/TelaMergeSort.java
@@ -1,29 +1,64 @@
 package gui.telas;
 
+import static algoritmos.MergeSort.mergeSort;
+import gui.componentesTela.EstadoOrdenacao;
+import gui.componentesTela.Grafico;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author cayke
  */
 public class TelaMergeSort implements InterfaceTela {
-
+    private int[] a;
+    private List<EstadoOrdenacao> copias;
+    private int copiaAtual;
+    private Grafico grafico;
+    
+    private double tempoParaMudar;
+    private double contadorTempo;
+    
     @Override
-    public void create () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void create() {
+        
+        copias = new ArrayList<>();
+        a = new int[]{9, 5, 4, 1, 2, 7, 6, 8, 3, 10};
+        copiaAtual = 0;
+        grafico = new Grafico();
+        
+        tempoParaMudar = 1;
+        contadorTempo = 0;
+        
+        mergeSort(a, copias);
+        System.out.println(copias.size());
     }
 
+    
     @Override
-    public void update ( double delta , TelaAtual janela ) {
-        throw new UnsupportedOperationException ( "Not supported yet." ); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void update( double delta, TelaAtual tela ) {
+        
+        contadorTempo += delta;
+        
+        if(contadorTempo >= tempoParaMudar){
+            if(copiaAtual< copias.size() -1 ){
+                copiaAtual++;
+            }
+            contadorTempo = 0;
+        }
+        
     }
-
+    
     @Override
-    public void draw ( TelaAtual janela ) {
-        throw new UnsupportedOperationException ( "Not supported yet." ); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void draw(TelaAtual tela) {
+        
+        grafico.desenharGrafico( tela , copias.get( copiaAtual ));
+    
     }
 
     @Override
     public String getTitulo () {
         return "Merge Sort";
     }
-    
+
 }
